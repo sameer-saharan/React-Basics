96941cbd5396aef29e506ae07782168fee351856
Sameer Saharan <albaadigoru@gmail.com>
2024-09-08 10:36:48 +0530

    added currentDate functionality

--- 06currencyConv/src/hooks/useCurrencyInfo.js
+++ 06currencyConv/src/hooks/useCurrencyInfo.js
@@ -1,9 +1,9 @@
 import {useState, useEffect} from "react"
 
-function useCurrencyInfo(currency) {
+function useCurrencyInfo(currency, date) {
     const [data, setData] = useState({});
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.9.5/v1/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${date}/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
     }, [currency])
