95c75c286abdeea0ad62af47ece6faeb6f3e4e62
Sameer Saharan <albaadigoru@gmail.com>
2024-09-08 11:48:48 +0530

    responsive app

--- 06currencyConv/src/components/InputField.jsx
+++ 06currencyConv/src/components/InputField.jsx
@@ -16,7 +16,7 @@ function InputField({
           <p className="mb-2 block">{label}</p>
           <input
             type="number"
-            className="outline-none bg-transparent py-1"
+            className="w-1/2 outline-none bg-transparent py-1 "
             placeholder="amount"
             value={amount}
             onChange={(e) =>
@@ -25,10 +25,10 @@ function InputField({
             disabled={amountDisable}
           />
         </div>
-        <div className="w-1/2 flex justify-end text-right flex-col">
-          <p className="">Currency Type</p>
+        <div className="w-1/2 flex items-end flex-col">
+          <p className="mb-2">Currency Type</p>
           <select
-            className="px-2 py-2 outline-none rounded-lg cursor-pointer"
+            className="w-1/2 px-2 py-2 outline-none rounded-lg cursor-pointer"
             value={selectCurrency}
             onChange={(e) =>
               onCurrencyChange && onCurrencyChange(e.target.value)
