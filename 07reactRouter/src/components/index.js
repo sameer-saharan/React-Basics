75917e5d80d26ea55f15252a97789f683069cc8d
Sameer Saharan <albaadigoru@gmail.com>
2024-09-10 08:34:18 +0530

    Exporting all components from this index file

---
+++ 07reactRouter/src/components/index.js
@@ -0,0 +1,8 @@
+import About from "./About/About.jsx"
+import Footer from "./Footer/Footer.jsx"
+import Header from "./Header/Header.jsx"
+import Home from "./Home/Home.jsx"
+import Github from "./Github/Github.jsx"
+import Contact from "./Contact/Contact.jsx"
+
+export {About, Footer, Header, Home, Github, Contact};
