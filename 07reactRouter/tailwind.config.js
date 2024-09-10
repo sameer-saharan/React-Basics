41dc03067671f76cce7664d6906e395ee150f7e6
Sameer Saharan <albaadigoru@gmail.com>
2024-09-10 11:17:39 +0530

    changed width

--- 07reactRouter/tailwind.config.js
+++ 07reactRouter/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'max-md': { 'max': '639px' }, 
+      },
+    },
   },
   plugins: [],
 }
