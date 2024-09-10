8bec7df4bbaf9632ddb539bcfbcf31c87f4e1722
Sameer Saharan <albaadigoru@gmail.com>
2024-09-10 08:32:52 +0530

    Outlet added

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { Outlet } from "react-router-dom";
+import { Footer, Header } from "./components/index";
 
 function App() {
-
   return (
     <>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
