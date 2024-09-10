433382cb022dfa8c81aa8b4bed433bb23aa9e06d
Sameer Saharan <albaadigoru@gmail.com>
2024-09-10 11:15:23 +0530

    Update: routing syntax

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,33 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import {About, Home, Github, Contact} from './components/index'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: <Home />
-      },
-      {
-        path: 'about',
-        element: <About />
-      },
-      {
-        path: 'github',
-        element: <Github />
-      },
-      {
-        path: 'contact',
-        element: <Contact />
-      },
-    ]
-  }
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<Home />}/>
+      <Route path='about' element={<About />}/>
+      <Route path='contact' element={<Contact />}/>
+      <Route path='github' element={<Github />}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
