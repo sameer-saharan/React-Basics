00f9b2d68ac0486f67da9dc210674f41ef8c1f25
Sameer Saharan <albaadigoru@gmail.com>
2024-09-28 19:02:23 +0530

    Test: 1.0 of react router

--- 01homepage/src/App.jsx
+++ 01homepage/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 function App() {
-
+  const projects = [
+    { name: 'Counter', path: '/02counter' },
+    { name: 'Background Changer', path: '/04bgChanger' },
+    { name: 'Password Generator', path: '/05passwordGen' },
+    { name: 'Currency Converter', path: '/06currencyConv' },
+    { name: 'React Router', path: '/07reactRouter' },
+  ]
   return (
     <>
-      <h1>React Beginner Projects</h1>
-      <div>
-        <a href="../../02counter/index.html">Counter Project</a>
-      </div>
+      <Router>
+        <h1>React Beginner Projects</h1>
+        <div>
+          <Link to={'/02counter'}>Counter</Link>
+        </div>
+
+        <Switch>
+          <Route path="/02counter" component={() => window.location.href = 'https://react-sameer.vercel.app/02counter'} />
+        </Switch>
+      </Router>
     </>
   )
 }
