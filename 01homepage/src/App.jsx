a8e4c0c22249aba03d0560a98c469c48d6c54dc3
Sameer Saharan <albaadigoru@gmail.com>
2024-09-28 18:52:02 +0530

    homepage init

---
+++ 01homepage/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Beginner Projects</h1>
+      <div>
+        <a href="../../02counter/index.html">Counter Project</a>
+      </div>
+    </>
+  )
+}
+
+export default App
