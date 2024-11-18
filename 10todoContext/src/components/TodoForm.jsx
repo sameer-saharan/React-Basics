005669d46db9ffa14213484b07a7e0b4428785db
Sameer Saharan <albaadigoru@gmail.com>
2024-11-18 12:33:07 +0530

    defined funciton: createTodo and handled form submission

--- 10todoContext/src/components/TodoForm.jsx
+++ 10todoContext/src/components/TodoForm.jsx
@@ -1,10 +1,26 @@
+import { useTodo } from "../context";
+import { useState } from "react";
+
 function TodoForm() {
+  const [todoTitle, setTodoTitle] = useState("");
+  const {globalId, addTodo} = useTodo();
+
+  const createTodo = (e) => {
+    e.preventDefault();
+    if (!todoTitle) return;
+
+    addTodo({id: globalId, todo: todoTitle, completed: false});
+    setTodoTitle("");
+  };
+
   return (
-    <form className="flex">
+    <form onSubmit={createTodo} className="flex">
       <input
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todoTitle}
+        onChange={(e) => setTodoTitle(e.target.value)}
       />
       <button
         type="submit"
