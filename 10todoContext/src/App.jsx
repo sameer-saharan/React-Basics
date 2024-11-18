882be25b895445db55feaa6431627000851cfeb5
Sameer Saharan <albaadigoru@gmail.com>
2024-11-18 12:30:48 +0530

    created global-id and added local storage fnc

--- 10todoContext/src/App.jsx
+++ 10todoContext/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TodoProvider} from "./context";
 import {TodoForm, TodoItem} from "./components"
 
 function App() {
+  const [globalId, setGlobalId] = useState(1);
   const [todos, setTodos] = useState([]);
 
+  // CRUD Operations
   const addTodo = (todo) => {
     setTodos((prevState) => [todo, ...prevState]);
+    setGlobalId(globalId+1);
   };
 
   const updateTodo = (id, todo) => {
@@ -23,19 +26,38 @@ function App() {
     )
   }
 
+  // Local Storage Functionality
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
   return (
     <>
-      <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <TodoProvider value={{globalId, todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
         <div className="bg-[#172842] min-h-screen py-8">
           <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">
               Manage Your Todos
             </h1>
             <div className="mb-4">
-              {/* Todo form goes here */}
+              <TodoForm />
             </div>
             <div className="flex flex-wrap gap-y-3">
-              {/*Loop and Add TodoItem here */}
+              {
+                todos.map((todo) => {
+                  <div key={todo.id} className='w-full' >
+                    <TodoItem todo={todo} />
+                  </div>    
+                })
+              }
             </div>
           </div>
         </div>
