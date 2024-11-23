fd8f4110e2c01b4e0131d7d47593bba672fe5e74
Sameer Saharan <albaadigoru@gmail.com>
2024-11-23 23:37:02 +0530

    imported components

--- 11reduxToolkit/src/App.jsx
+++ 11reduxToolkit/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import ListTodos from './components/ListTodos'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div className='w-full max-w-screen-sm mx-auto py-5'>
+      <h1 className='text-6xl text-center font-bold pb-10 text-white'>Redux Toolkit</h1>
+      <AddTodo />
+      <ListTodos />     
+    </div>
   )
 }
 
