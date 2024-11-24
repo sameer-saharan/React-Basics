bf0f08a664d41b3cfd2a48fd1432c498b9442ae1
Sameer Saharan <albaadigoru@gmail.com>
2024-11-24 20:23:14 +0530

    environment variables and config

---
+++ MegaBlog/src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    ProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default config;
