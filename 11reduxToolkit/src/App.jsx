de2d29790efa6cbdb980cc7ecc33eea991a2887a
Sameer Saharan <albaadigoru@gmail.com>
2024-11-26 16:29:23 +0530

    imported TodoItem.jsx and passing each todo as a prop

--- 11reduxToolkit/src/App.jsx
+++ 11reduxToolkit/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
 import AddTodo from './components/AddTodo'
-import ListTodos from './components/ListTodos'
+import TodoItem from './components/TodoItem'
+import { useSelector } from 'react-redux'
 
 function App() {
+  const todos = useSelector((state) => state.todos);
 
   return (
     <div className='w-full max-w-screen-sm mx-auto py-5'>
       <h1 className='text-6xl text-center font-bold pb-10 text-white'>Redux Toolkit</h1>
       <AddTodo />
-      <ListTodos />     
+      <div className='flex flex-col gap-y-5 justify-center px-16 py-10'>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo}/>
+        ))
+        }
+      </div>    
     </div>
   )
 }
