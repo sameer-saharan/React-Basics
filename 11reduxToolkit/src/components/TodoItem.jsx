d487a79e9ffd3547d47aa6a08d730612616af901
Sameer Saharan <albaadigoru@gmail.com>
2024-11-26 16:23:25 +0530

    New Component: TodoItem.jsx to perform CRUD operation on a selected Todo

---
+++ 11reduxToolkit/src/components/TodoItem.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { removeTodo, updateTodo, markAsDone } from '../features/todo/todoSlice';
+
+function TodoItem({todo}) {
+    const [todoText, setTodoText] = useState(todo.text);
+    const [isEditable, setEditable] = useState(false);
+    const [isCompleted, setCompleted] = useState(false);
+
+    const dispatch = useDispatch();    
+    const editTodo = (id) => {
+        dispatch(updateTodo({id: id, text: todoText}));
+    }
+
+  return (
+
+    <div className={`w-full flex flex-col p-2 rounded-lg font-semibold ${isCompleted ? 'bg-gray-600' : 'bg-yellow-200' }`}>
+        <input 
+        type="text"
+        className={`text-xl bg-transparent outline-none p-1 ${isEditable ? 'shadow-sm shadow-gray-500 rounded-md' : ''} ${isCompleted ? 'line-through' : ''}`}
+        value={todoText}
+        readOnly={!isEditable}
+        onChange={(e) => setTodoText(e.target.value)}
+        />
+        <div className='flex justify-end gap-x-3 pt-2'>
+            <button
+            className={` px-2 text-lg outline-none rounded-md font-semibold ${isEditable ? 'cursor-not-allowed bg-gray-500 hover:bg-gray-500' : 'bg-green-400 hover:bg-green-500'}`}
+            onClick={() => {
+                setCompleted(!isCompleted);
+                dispatch(markAsDone(todo.id));
+            }}
+            disabled={isEditable}
+            >
+                {isCompleted ? 'Undo' : 'Done'}
+            </button>
+
+            <button
+            className={`px-2 text-lg outline-none rounded-md  font-semibold ${isCompleted ? 'cursor-not-allowed bg-gray-500 hover:bg-gray-500' : 'bg-orange-400 hover:bg-orange-500'}`}
+            disabled={isCompleted}
+            onClick={() => {             
+                setEditable(false);
+
+                if (isEditable) {
+                    editTodo(todo.id);
+                } else {
+                    setEditable(!isEditable)
+                }
+                
+            }}>
+                {isEditable ? 'Save' : 'Edit'}
+            </button>
+
+            <button
+            className='bg-red-400 px-2 text-lg outline-none rounded-md hover:bg-red-500 font-semibold'
+            onClick={() => dispatch(removeTodo(todo.id))}>
+                Delete
+            </button>
+        </div>
+            
+    </div>
+  )
+}
+
+export default TodoItem
