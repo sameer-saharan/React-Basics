ba9f70200489b006bedaab642d68b2a15754ce76
Sameer Saharan <albaadigoru@gmail.com>
2024-12-14 07:40:26 +0530

    NEW: added react router

--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -1,9 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom"
+import {Provider} from "react-redux"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='' element={<App />}>
+      {/* <Route path='/' element={}/> */}
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
-
-  <App />
-
+  <RouterProvider router={router}/>
+  // <Provider>
+  // </Provider>
 )
