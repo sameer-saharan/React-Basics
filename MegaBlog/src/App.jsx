697750a173afe41d5413e8e53346197b7e1d9bc9
Sameer Saharan <albaadigoru@gmail.com>
2024-12-14 07:39:51 +0530

    Layout setup

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
+import { Outlet } from "react-router-dom"
 
 function App() {
 
   return (
-   <div className='w-full max-w-xs mx-auto h-screen responsive'>
-   </div>
+    <div className="w-full bg-[#232323]"> 
+
+      <div className="w-full sticky top-0 h-5 bg-slate-500">
+        {/* Header */}
+      </div>
+
+      <div className='w-full max-w-screen-sm mx-auto min-h-screen px-5 responsive'>
+        <Outlet />
+      </div>
+
+      <div className="w-full h-5 bg-slate-500">
+        {/* Footer */}
+      </div>
+    </div>
+   
   )
 }
 
