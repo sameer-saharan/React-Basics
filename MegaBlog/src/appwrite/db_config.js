114e979c2ffa15aad55a85b88987653b920b297b
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-18 22:14:50 +0530

    FEAT: CRUD operations

--- MegaBlog/src/appwrite/db_config.js
+++ MegaBlog/src/appwrite/db_config.js
@@ -1,5 +1,5 @@
 import config from "../config/config.js";
-import { Client, Databases, Storage} from "appwrite";
+import { Client, Databases, Storage } from "appwrite";
 
 export class DatabaseService {
     client = new Client();
@@ -14,6 +14,56 @@ export class DatabaseService {
         this.databases = new Databases(this.client);
         this.storage = new Storage(this.client);
     }
+
+    async createPost({slug, title, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            );
+        } catch (error) {
+            console.log("Appwrite/createPost : ", error);
+        }
+    };
+
+    async updatePost(slug, {title, content, featuredImage, status}) {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            );
+        } catch (error) {
+            console.log("Appwrite/updatePost : ", error);
+        }
+    };
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+            );
+            return true;
+        } catch (error) {
+            console.log("Appwrite/deletePost : ", error);
+        }
+    };
 
     
 }
