c8911594f35fdd44fcdec0c59feea117d65db83b
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-22 21:33:00 +0530

    FIX: 07reactRouter/About.jsx - img url and width

--- 07reactRouter/src/components/About/About.jsx
+++ 07reactRouter/src/components/About/About.jsx
@@ -4,10 +4,11 @@ import {Link, NavLink} from "react-router-dom"
 function About() {
     return (
         <>
-            <div className="mx-auto flex flex-col items-center px-7 py-4 gap-1 md:flex-row md:justify-center md:py-20">
+            <div className="mx-auto flex flex-col items-center px-7 py-4 gap-5 sm:gap-20 md:flex-row md:justify-center md:py-20">
                 <div className="">
                     <img
-                    src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                    src="https://images.pexels.com/photos/52608/pexels-photo-52608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    className="w-96 rounded-md"
                     alt="image"
                     />
                 </div>
