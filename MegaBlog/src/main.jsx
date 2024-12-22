ac9726d71023af3abd3055410fc6805f867a6cdb
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-22 21:10:32 +0530

    ADD: wrap RouterProvider under react-redux Provider and set default Route path for pages/Home.jsx

--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -1,19 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom"
-import {Provider} from "react-redux"
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./redux/store.js";
+import {Home} from "./pages/Home.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='' element={<App />}>
-      {/* <Route path='/' element={}/> */}
+      <Route path='/' element={<Home />}/>
     </Route>
   )
 )
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
-  // <Provider>
-  // </Provider>
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
 )
