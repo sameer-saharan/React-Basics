e5271a8dc34d9e62227885a89926f5cbce1acdc4
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-23 22:17:25 +0530

    UPDATE: Header's parent div style in App.jsx

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,12 +1,12 @@
-import { Outlet } from "react-router-dom"
-import {Header, Footer} from "./layout"
+import { Outlet } from "react-router-dom";
+import {Header, Footer} from "./layout";
 
 function App() {
 
   return (
-    <div className="w-full bg-[#232323]"> 
+    <div className="w-full m-0 bg-[#f1f1f1] dark:bg-[#232323]"> 
 
-      <div className="w-full sticky top-0 h-5 bg-slate-500">
+      <div className="w-full shadow-md sticky top-0 px-5 py-5 dark:bg-[#242424]">
         {<Header />}
       </div>
 
@@ -14,7 +14,7 @@ function App() {
         <Outlet />
       </div>
 
-      <div className="w-full h-5 bg-slate-500">
+      <div className="w-full py-5 bg-slate-500">
         {<Footer />}
       </div>
     </div>
