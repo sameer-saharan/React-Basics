4e6b5c8ea15627c64e7be3cacae074571609fe98
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-23 13:15:17 +0530

    ADD: themeReducer import in store

--- MegaBlog/src/redux/store.js
+++ MegaBlog/src/redux/store.js
@@ -1,10 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice"; 
-
+import themeReducer from "../features/theme/themeSlice";
 export const store = configureStore(
     {
         reducer: {
-            auth: authReducer
+            auth: authReducer,
+            theme: themeReducer,
         }
     }
 );
