95ada5e4b87ab33bb55e1069263527376e0fe0f8
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-26 08:50:07 +0530

    REFACTOR: shift theme-switch feat to ThemeButton.jsx

--- MegaBlog/src/layout/Header/Header.jsx
+++ MegaBlog/src/layout/Header/Header.jsx
@@ -1,37 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { toggleThemeMode } from '../../features/theme/themeSlice';
+import {ThemeButton} from "../../components"
+import LogoutButton from "./LogoutButton";
+import { useSelector } from 'react-redux';
+import {useNavigate} from "react-router-dom";
 
 function Header() {
-  const [currTheme, setCurrTheme] = useState("dark");
-  const dispatch = useDispatch();
-
-  const toggleThemeHandler = () => {
-    setCurrTheme((prevTheme) => prevTheme == "dark" ? "light" : "dark");
-
-    dispatch(toggleThemeMode(currTheme));
-  };
-
-  useEffect(() => {
-    document.querySelector('html').classList.remove('light', 'dark');
-    document.querySelector('html').classList.add(currTheme);
-  }, [currTheme]);
+  const navigate = useNavigate(); 
+  const authStatus = useSelector((state) => state.auth.status);
+  
+  const navItems = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: true,
+    },
+    {
+      name: 'Login',
+      slug: '/login',
+      active: !authStatus,
+    },
+    {
+      name: 'Signup',
+      slug: '/signup',
+      active: !authStatus,
+    },
+    {
+      name: 'Posts',
+      slug: '/posts',
+      active: authStatus,
+    },
+    {
+      name: 'Create Post',
+      slug: '/create',
+      active: authStatus
+    },
+  ];
 
   return (
     <div className='w-full max-w-screen-sm mx-auto px-5 responsive'>
       <div></div>
       
       <div className='flex gap-x-5'>
-        <button className='dark:text-white'>
-          Login
-        </button>
-        
-        <button 
-        onClick={toggleThemeHandler}
-        className='rounded-full p-5 transition-all ease-in-out'
-        style={{backgroundImage: `url(${currTheme == 'light' ? '/dark-theme.svg' : '/light-theme.svg'})`, backgroundSize: 'cover', backgroundPosition: 'center'}}
-        >
-        </button>
+        <ThemeButton />
       </div>
     </div>
   )
