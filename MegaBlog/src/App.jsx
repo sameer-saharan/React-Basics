12cd1dc38e719dee9cc3fa9dfca8e30750ce9592
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-28 16:03:37 +0530

    STYLE: dark mode classes and minor change in padding and hex

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -4,9 +4,9 @@ import {Header, Footer} from "./layout";
 function App() {
 
   return (
-    <div className="w-full m-0 bg-[#f1f1f1] dark:bg-[#232323]"> 
+    <div className="w-full m-0 bg-[#f1f1f1] dark:bg-[#1A1A1A]"> 
 
-      <div className="w-full shadow-md sticky top-0 px-5 py-5 dark:bg-[#242424]">
+      <div className="w-full shadow-sm dark:shadow-sm dark:shadow-[#252525] sticky top-0 px-5 py-1 bg-white dark:bg-[#1A1A1A]">
         {<Header />}
       </div>
 
