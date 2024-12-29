0c4935e244b243b375abe79c8b6337969f9e00f2
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-29 21:53:17 +0530

    REFACTOR: change type of label prop, object => string and adjusted code according to string

--- MegaBlog/src/components/Common/Input.jsx
+++ MegaBlog/src/components/Common/Input.jsx
@@ -3,24 +3,24 @@ import React, {forwardRef, useId} from 'react'
 const Input = forwardRef(({
     type = "text",
     className = "",
-    label, // ={text, style}
+    label,
     ...props
 
 }, ref) => {
     const id = useId();
     return (
-        <div>
+        <div className='flex flex-col gap-y-2'>
             {label && <label 
             htmlFor={id}
-            className={`${label.style}`}
+            className={`text-sm text-[#4b4b4b] dark:text-[#ececec] font-bold`}
             >
-                {label.text}
+                {label}
             </label>
             }
             <input 
             id={id}
             type={type} 
-            className={`${className}`}
+            className={`sm:w-80 rounded-md px-2 py-1 font-semibold dark:bg-[#e4e4e4] dark:text-black outline-none ring-[#85bdfd] dark:ring-[#0077ff] focus:ring-2 ${className}`}
             ref={ref}
             {...props} 
             />
