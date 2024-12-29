804c971ee753f594624e3d72be2d7af6f37c04a0
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-29 12:39:50 +0530

    STYLE: Header parent div bg-color hex-141414

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="w-full m-0 bg-[#f1f1f1] dark:bg-[#1A1A1A]"> 
 
-      <div className="w-full shadow-sm dark:shadow-sm dark:shadow-[#252525] sticky top-0 px-5 py-1 bg-white dark:bg-[#1A1A1A]">
+      <div className="w-full shadow-sm dark:shadow-sm dark:shadow-[#252525] sticky top-0 px-5 py-1 bg-white dark:bg-[#141414]">
         {<Header />}
       </div>
 
