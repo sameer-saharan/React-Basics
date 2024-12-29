c42746a6f99c9c08fd9a8d9081a0bed8ef2c0442
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-29 21:54:12 +0530

    ADD: export new pages and layouts

--- MegaBlog/src/layout/index.js
+++ MegaBlog/src/layout/index.js
@@ -1,4 +1,5 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import LogoutButton from "./Header/LogoutButton";
 
-export {Header, Footer};
+export {Header, Footer, LogoutButton};
