ccbe3c85dd5ff25bdf21b0fa23f57d0c8a04b93c
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-12-29 21:55:08 +0530

    ADD: routes /login and /singup

--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx';
 import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./redux/store.js";
-import {Home} from "./pages"
+import {Home, Login, Signup} from "./pages"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='' element={<App />}>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+
     </Route>
   )
 )
