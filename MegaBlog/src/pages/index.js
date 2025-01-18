a8c59ea8b515bd7a20c55a6d8aa90dea31190ac7
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-18 14:35:23 +0530

    ADD: /pages imports and exports

--- MegaBlog/src/pages/index.js
+++ MegaBlog/src/pages/index.js
@@ -1,5 +1,9 @@
 import Home from "./Home";
 import Login from "./Login";
 import Signup from "./Signup";
+import AllPosts from "./AllPosts";
+import EditPost from "./EditPost";
+import AddPost from "./AddPost";
+import ViewPost from "./ViewPost"
 
-export {Home, Login, Signup};
+export {Home, Login, Signup, AllPosts, EditPost, AddPost, ViewPost};
