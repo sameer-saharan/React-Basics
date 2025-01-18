aadc92e0d174797fc19b8d6c27c0101e739fd678
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-18 14:32:19 +0530

    ADD: postReducer

--- MegaBlog/src/redux/store.js
+++ MegaBlog/src/redux/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice"; 
 import themeReducer from "../features/theme/themeSlice";
+import postReducer from "../features/post/postSlice";
 export const store = configureStore(
     {
         reducer: {
             auth: authReducer,
             theme: themeReducer,
+            post: postReducer,
         }
     }
 );
