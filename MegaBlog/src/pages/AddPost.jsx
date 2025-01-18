a740ad0a40d383be215eb8237839989a580cefd5
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-18 14:38:04 +0530

    NEW: /pages AddPost.jsx

---
+++ MegaBlog/src/pages/AddPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PostForm } from "../components"
+
+function AddPost() {
+  return (
+    <div className='w-full py-20'>
+      <PostForm />
+    </div>
+  )
+}
+
+export default AddPost;
