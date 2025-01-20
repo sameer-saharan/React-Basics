4c555c5b2171cac4dd412561b05098ee59769ba3
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-20 14:20:29 +0530

    STYLE: major change in PostCard

--- MegaBlog/src/components/PostCard.jsx
+++ MegaBlog/src/components/PostCard.jsx
@@ -6,12 +6,13 @@ function PostCard({ $id, title, featuredImage }) {
   return (
     <>
       <Link to={`/posts/${$id}`}>
-        <div className="">
+        <div className=" bg-[#bdbdbd] dark:bg-[#2b2b2b] p-5 rounded-lg">
           <div>
-            <img src={dbService.getFilePreview(featuredImage)} 
+            <img className="rounded-sm hover:shadow-black hover:shadow-md dark:shadow-[#d3d3d3] transition-all ease-in-out" src={dbService.getFilePreview(featuredImage)} 
             />
           </div>
-          <h2 className="text-3xl dark:text-white">{title}</h2>
+          <div className='mt-3 h-[1px] bg-[#000000] dark:bg-[#f1f1f1]'></div>
+          <h2 className="text-md sm:text-lg md:text-xl font-mono dark:text-white">{title}</h2>
         </div>
       </Link>
     </>
