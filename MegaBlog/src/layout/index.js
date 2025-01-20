da609410f64f01e92f07e6a21c46078c570d53e9
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-20 14:19:26 +0530

    ADD: /layout import export of BottomNavBar

--- MegaBlog/src/layout/index.js
+++ MegaBlog/src/layout/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import LogoutButton from "./Header/LogoutButton";
+import BottomNavBar from "./Header/BottomNavBar";
 
-export {Header, Footer, LogoutButton};
+export {Header, Footer, LogoutButton, BottomNavBar};
