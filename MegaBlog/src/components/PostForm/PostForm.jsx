2f54cede813d5eac856ff96076d7d1c479ce5174
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-20 14:23:20 +0530

    FIX: Select label text & STYLE: label and parent div

--- MegaBlog/src/components/PostForm/PostForm.jsx
+++ MegaBlog/src/components/PostForm/PostForm.jsx
@@ -64,8 +64,8 @@ function PostForm({post}) {
     }, [watch, slugTransform, setValue]);
 
   return (
-    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
-        <div className="w-2/3 px-2">
+    <form onSubmit={handleSubmit(submit)} className="flex flex-col gap-5">
+        <div className="w-full px-2">
             <Input
             label="Title :"
             placeholder="Title"
@@ -83,11 +83,11 @@ function PostForm({post}) {
             />
             <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
         </div>
-        <div className="w-1/3 px-2">
+        <div className="w-full px-2">
             <Input
             label="Featured Image :"
             type="file"
-            className="mb-4"
+            className="mb-4 text-sm"
             accept="image/png, image/jpg, image/jpeg, image/gif"
             {...register("image", { required: !post })}
             />
@@ -102,11 +102,11 @@ function PostForm({post}) {
             )}
             <Select
                 options={["active", "inactive"]}
-                label="Status"
-                className="mb-4"
+                label={{text: "Status : ", style: "font-semibold dark:text-white"}}
+                className="px-2 py-1 mb-5 rounded-lg"
                 {...register("status", { required: true })}
             />
-            <Button text={post ? "Update" : "Submit"} type="submit" className="w-full" />
+            <Button text={post ? "Update" : "Submit"} type="submit" className={'w-1/3 px-2 py-1 rounded-md'} />
         </div>
     </form>
   );
