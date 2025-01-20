74f547ca73c37a7d51bdd8f8b410469a39fbd14e
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-20 14:18:44 +0530

    NEW: BottomNavBar.jsx - navigation bar for mobile screens

---
+++ MegaBlog/src/layout/Header/BottomNavBar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ThemeButton } from '../../components';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+function BottomNavBar() {
+const authStatus = useSelector((state) => state.auth.status);
+const navigate = useNavigate(); 
+  const navItems = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: authStatus,
+    },
+    {
+      name: 'Create Post',
+      slug: '/add',
+      active: authStatus
+    },
+  ];
+
+  return (
+    <div className={`${authStatus ? "" : "hidden"} sm:hidden fixed bottom-0 w-full bg-white dark:bg-[#141414] dark:text-white border-t border-gray-300 dark:border-gray-700`}>
+      <div className="flex justify-evenly py-2">
+        <ul className="flex gap-x-10 list-none">
+          {navItems.map((item, index) =>
+            item.active ? (
+              <li key={`nav-li-${index}`} className="dark:text-white">
+                <button
+                  className="font-bold text-sm hover:text-orange-400 dark:hover:text-yellow-300 transition-all ease-in-out"
+                  onClick={() => navigate(item.slug)}
+                >
+                  {item.name}
+                </button>
+              </li>
+            ) : null
+          )}
+        </ul>
+        <ThemeButton />
+      </div>
+    </div>
+  );
+}
+
+export default BottomNavBar;
+
