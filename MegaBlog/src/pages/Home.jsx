3c5cd3ec28137bbbcae87aca3aa8f9e15cbb3cb9
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-20 14:27:12 +0530

    UPDATE: Home.jsx first

--- MegaBlog/src/pages/Home.jsx
+++ MegaBlog/src/pages/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useDispatch } from "react-redux";
+import authService from "../appwrite/auth";
+import { userLogin, userLogout } from "../features/auth/authSlice";
+import {AllPosts} from './index';
 
 function Home() {
-  return (
-    <div>Home</div>
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(userLogin(userData));
+      } else {
+        dispatch(userLogout());
+      }
+    })
+    .finally(() => setLoading(false));
+  }, []);
+
+  return loading ? (
+    <div>
+      {/* While Loading.. */}
+    </div>
+  ) : (
+    <div className='w-full py-10'>
+      <AllPosts />
+    </div>
   )
 }
 
-export default Home
+export default Home;
