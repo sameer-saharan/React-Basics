eae41c91cfb5edc51caeb599b1278de4e887f0d9
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-20 14:26:58 +0530

    STYLE: PostCard center with wrap and width minor change

--- MegaBlog/src/pages/AllPosts.jsx
+++ MegaBlog/src/pages/AllPosts.jsx
@@ -18,9 +18,9 @@ function AllPosts() {
 
   return (
   <div className='w-full py-8'>
-    <div className='flex flex-wrap'>
+    <div className='flex flex-wrap justify-center gap-5 sm:gap-20'>
       {posts.map((post) => (
-        <div key={post.$id} className='p-2 w-1/4'>
+        <div key={post.$id} className='p-2 min-w-64 w-64 lg:w-80'>
           <PostCard {...post} />
         </div>
       ))}
