1c13b8f5e1a95c2876815a9d013e995b88842662
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-20 17:38:18 +0530

    STYLE: opacity-0.5 if post status is inactive

--- MegaBlog/src/components/PostCard.jsx
+++ MegaBlog/src/components/PostCard.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import dbService from "../appwrite/db_config";
 import { Link } from "react-router-dom";
 
-function PostCard({ $id, title, featuredImage }) {
+function PostCard({ $id, title, featuredImage, userName, status }) {
   return (
     <>
       <Link to={`/posts/${$id}`}>
-        <div className=" bg-[#bdbdbd] dark:bg-[#2b2b2b] p-5 rounded-lg">
+        <div className={ `${status ? "" : "opacity-[0.5]"} p-5 rounded-lg bg-[#bdbdbd] dark:bg-[#2b2b2b]`}>
           <div>
             <img className="rounded-sm hover:shadow-black hover:shadow-md dark:shadow-[#d3d3d3] transition-all ease-in-out" src={dbService.getFilePreview(featuredImage)} 
             />
           </div>
           <div className='mt-3 h-[1px] bg-[#000000] dark:bg-[#f1f1f1]'></div>
           <h2 className="text-md sm:text-lg md:text-xl font-mono dark:text-white">{title}</h2>
+          <h3 className="text-sm font-bold dark:text-white">by {userName}</h3>
         </div>
       </Link>
     </>
