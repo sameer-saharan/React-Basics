1535093199ab61848fac50265486dc45a16f7d24
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-21 15:01:25 +0530

    ADD: path to svg and minor style changes

--- MegaBlog/src/layout/Header/BottomNavBar.jsx
+++ MegaBlog/src/layout/Header/BottomNavBar.jsx
@@ -5,20 +5,24 @@ import { useSelector } from 'react-redux';
 
 function BottomNavBar() {
 const authStatus = useSelector((state) => state.auth.status);
+const currTheme = useSelector((state) => state.theme.themeMode);
 const navigate = useNavigate(); 
   const navItems = [
     {
-      name: 'Home',
+      path: 'home-icon-black.svg',
+      dark: 'home-icon-white.svg',
       slug: '/',
       active: authStatus,
     },
     {
-      name: 'Posts',
+      path: 'user-icon-black.svg',
+      dark: 'user-icon-white.svg',
       slug: '/posts',
       active: authStatus
     },
     {
-      name: 'Create Post',
+      path: 'create-icon-black.svg',
+      dark: 'create-icon-white.svg',
       slug: '/add',
       active: authStatus
     },
@@ -26,13 +30,14 @@ const navigate = useNavigate();
 
   return (
     <div className={`${authStatus ? "" : "hidden"} sm:hidden fixed bottom-0 w-full bg-white dark:bg-[#141414] dark:text-white border-t border-gray-300 dark:border-gray-700`}>
-      <div className="flex justify-evenly py-2">
+      <div className="flex justify-center py-2">
         <ul className="flex gap-x-10 list-none">
           {navItems.map((item, index) =>
             item.active ? (
               <li key={`nav-li-${index}`} className="dark:text-white">
                 <button
-                  className="font-bold text-sm hover:text-orange-400 dark:hover:text-yellow-300 transition-all ease-in-out"
+                  style={{backgroundImage: `url(${currTheme == 'light' ? `/${item.path}` : `/${item.dark}`})`, backgroundSize: 'cover', backgroundPosition: 'center'}}
+                  className="p-3 transition-all ease-in-out hover:bg-[#d5d2d2] dark:hover:bg-[#3f3e3e] hover:rounded-sm"
                   onClick={() => navigate(item.slug)}
                 >
                   {item.name}
@@ -40,8 +45,10 @@ const navigate = useNavigate();
               </li>
             ) : null
           )}
+          <li>
+            <ThemeButton />
+          </li>
         </ul>
-        <ThemeButton />
       </div>
     </div>
   );
