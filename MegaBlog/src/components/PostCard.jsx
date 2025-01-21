5c709e01f6f62b4dd8b858abfad8e43e167d9b29
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-21 13:59:06 +0530

    STYLE: PostCard dark:shadow color

--- MegaBlog/src/components/PostCard.jsx
+++ MegaBlog/src/components/PostCard.jsx
@@ -8,7 +8,7 @@ function PostCard({ $id, title, featuredImage, userName, status }) {
       <Link to={`/posts/${$id}`}>
         <div className={ `${status ? "" : "opacity-[0.5]"} p-5 rounded-lg bg-[#bdbdbd] dark:bg-[#2b2b2b]`}>
           <div>
-            <img className="rounded-sm hover:shadow-black hover:shadow-md dark:shadow-[#d3d3d3] transition-all ease-in-out" src={dbService.getFilePreview(featuredImage)} 
+            <img className="rounded-sm hover:shadow-black hover:shadow-md dark:shadow-[#FDF3C5] transition-all ease-in-out" src={dbService.getFilePreview(featuredImage)} 
             />
           </div>
           <div className='mt-3 h-[1px] bg-[#000000] dark:bg-[#f1f1f1]'></div>
