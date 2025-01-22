2c86a7b4a2d2f2b05872909edf16117259ffb012
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-01-22 23:14:36 +0530

    REFACTOR: authentication logic

--- MegaBlog/src/components/AuthLayout.jsx
+++ MegaBlog/src/components/AuthLayout.jsx
@@ -2,20 +2,19 @@ import React, {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-function AuthLayout({children, authentication}) {
+function AuthLayout({children, authentication = true}) {
     const navigate = useNavigate();
     const authStatus = useSelector((state) => state.auth.status);
     const [loader, setLoader] = useState(true);
 
     useEffect(() => {
-        if (authStatus) {
-            navigate("/");
-        } else if (!authStatus) {
-            navigate("/login");
+        if(authentication && authStatus !== authentication){
+            navigate("/login")
+        } else if(!authentication && authStatus !== authentication){
+            navigate("/")
         }
-        setLoader(false);
-    }, 
-    [authStatus, navigate]);
+        setLoader(false)
+    }, [authStatus, navigate, authentication]);
 
   return loader ? (<h1>Please Wait...</h1>) : (<>{children}</>);
 }
